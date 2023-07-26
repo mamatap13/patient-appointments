3196f4ee4adc60a41e3acfef59d433a476499c6c
mamatap13 <mamatapolisetty@gmail.com>
2023-07-25 23:14:02 -0700

    PatientException handling

---
+++ src/main/java/com/example/patientappointments/exceptions/PatientException.java
@@ -0,0 +1,7 @@
+package com.example.patientappointments.exceptions;
+
+public class PatientException extends RuntimeException {
+    public PatientException(String message) {
+        super(message);
+    }
+}
